089df0f74fe866d729ecfec6d020d58bf051b2a9
Jez-er <103408350+Jez-er@users.noreply.github.com>
2026-04-05 12:27:55 +0300

    Update course.module.ts

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CourseService } from './course.service.js';
 import { CoursePrivateController } from './course.private.controller.js';
 import { PrismaModule } from '../modules/Prisma/prisma.module.js';
-import { CoursePublicController } from './course.pulic.controller.js';
+import { CoursePublicController } from './course.public.controller.js';
 
 @Module({
   imports: [PrismaModule],
